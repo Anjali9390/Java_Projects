6aed4664f9e4a12b82b0fabd949a36d76a6345e3
Anjali Talluri <anjalithalluri9390@gmail.com>
2025-06-18 11:50:53 +0530

    Initial commit of Student Management project

---
+++ src/main/java/com/example/Student_Management/StudentController.java
@@ -0,0 +1,41 @@
+package com.example.Student_Management;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private StudentService service;
+
+    @GetMapping
+    public String getAllStudents() {
+        
+        return "Student list working!";
+    }
+
+    @GetMapping("/{id}")
+    public Student getStudentById(@PathVariable Long id) {
+        return service.getStudentById(id);
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return service.addStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        service.deleteStudent(id);
+    }
+}
---
+++ src/main/java/com/example/Student_Management/StudentManagementApplication2.java
@@ -0,0 +1,14 @@
+package com.example.Student_Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication2
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication2.class, args);
+	}
+
+}
